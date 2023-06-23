2d2cb17ffdedb6092b84ef01c9579222d867483b
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2023-06-22 21:01:30 -0300

    feat(front/components): project structure wip

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,6 +1,20 @@
+import { Content } from "@/components/common/content";
+import { Footer } from "@/components/common/footer";
+import { Header } from "@/components/common/header";
+import { MainContent } from "@/components/common/main-content";
+import { NavigationMenu } from "@/components/common/navigation-menu";
+
 const Home = () => {
   return (
     <>
+      <Header />
+      <NavigationMenu />
+      <main>
+        <Content />
+        <MainContent />
+        <Content />
+      </main>
+      <Footer />
     </>
   )
 };
